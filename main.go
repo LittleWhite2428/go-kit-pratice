53e10fcbf117c1490dfd933a0ec9f35bbbff3721
王昱超 <wangyuchao@wangyuchaodeMacBook-Pro-2.local>
2022-03-17 23:56:14 +0800

    加入mux包，三方形式进行rust风格路由注册

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	httptransport "github.com/go-kit/kit/transport/http"
 	"go-kit-exercise/service"
+	mymux "github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -11,6 +12,8 @@ func main() {
 	endp := service.GenUserEndpoint(user)
 
 	serviceHandler := httptransport.NewServer(endp,service.DecodeUserRequest,service.EncodeUserResponse)
-	http.ListenAndServe(":8080",serviceHandler)
+	r := mymux.NewRouter()
+	r.Handle(`/user/{uid:\d+}`,serviceHandler)
+	http.ListenAndServe(":8080",r)
 
 }
--- service/UserTransport.go
+++ service/UserTransport.go
@@ -4,13 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 )
 
 func DecodeUserRequest(c context.Context,r *http.Request) (interface{},error) {
-	if r.URL.Query().Get("uid") != ""{
-		uid,_ := strconv.Atoi(r.URL.Query().Get("uid"))
+	//if r.URL.Query().Get("uid") != ""{
+	//	uid,_ := strconv.Atoi(r.URL.Query().Get("uid"))
+	//	return UserRequest{
+	//		Uid: uid,
+	//	},nil
+	//}
+	vars := mux.Vars(r)
+	if uid, ok := vars["uid"];ok{
+		uid, _ := strconv.Atoi(uid)
 		return UserRequest{
 			Uid: uid,
 		},nil
