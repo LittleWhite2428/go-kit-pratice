ea6c6aa161fe78aaa1b8af1bbebb32ef6ed1252e
王昱超 <wangyuchao@wangyuchaodeMacBook-Pro-2.local>
2022-03-21 00:00:16 +0800

    新增健康检查

--- main.go
+++ main.go
@@ -12,8 +12,15 @@ func main() {
 	endp := service.GenUserEndpoint(user)
 
 	serviceHandler := httptransport.NewServer(endp,service.DecodeUserRequest,service.EncodeUserResponse)
-	r := mymux.NewRouter()
-	r.Handle(`/user/{uid:\d+}`,serviceHandler)
-	http.ListenAndServe(":8080",r)
+	router := mymux.NewRouter()
+	{
+		router.Handle(`/user/{uid:\d+}`,serviceHandler)
+		router.Methods("GET").Path("/heath").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			writer.Header().Set("Contend-type","application")
+			writer.Write([]byte(`{"status:OK"}`))
+		})
+	}
+
+	http.ListenAndServe(":8080", router)
 
 }
